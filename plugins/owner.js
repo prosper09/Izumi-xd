8f964228eacee5d44922869c82dc392b72d87e49
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-24 01:44:03 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,19 +9,19 @@ command(
   async (message, match, m, client) => {
   const zeta = {
   'contacts': {
-    'displayName': "𝐙𝐞𝐭𝐚-𝐗𝐃",
+    'displayName': "𝙀𝙮𝙥𝙯",
     'contacts': [{
-      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝐙𝐄𝐓𝐀-𝐗𝐃\nORG:Zeta-XD;\nTEL;type=CELL;type=VOICE;waid=919747257996:919747257996\nEND:VCARD"
+      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝙀𝙮𝙥𝙯\nORG:Eypz\nTEL;type=CELL;type=VOICE;waid=919747257996:919747257996\nEND:VCARD"
     }]
   },
   'contextInfo': {
     'externalAdReply': {
-      'title': "𝐙𝐞𝐭𝐚𝐚𝐡𝐡 ⛮",
-      'body': "ᴢᴇᴛᴀ-xᴅ ᴅᴇᴠᴇʟᴏᴩᴇʀ",
-      'thumbnailUrl': "https://i.imgur.com/xJV2r3g.jpeg",
+      'title': "𝙀𝙮𝙥𝙯 ⛮",
+      'body': "𝙀𝙮𝙥𝙯/🌸```",
+      'thumbnailUrl': "https://i.imgur.com/AaP7a7m.jpeg",
       'mediaType': 0x1,
-      'mediaUrl': "http://wa.me/+919747257996?text=Hi+Badan-Ser+Im+From+Zeta-XD+Git",
-      'sourceUrl': "http://wa.me/+919747257996?text=Hi+Badan-Ser+Im+From+Zeta-XD+Git",
+      'mediaUrl': "http://wa.me/+917994489493?text=Hey",
+      'sourceUrl': "http://wa.me/+917994489493?text=Hey",
       'showAdAttribution': false
     }
   }
