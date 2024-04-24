a7691636a9e988f77c577f0ed78f950695b691d7
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-24 14:57:50 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,7 @@ command(
   'contacts': {
     'displayName': "𝙀𝙮𝙥𝙯",
     'contacts': [{
-      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝙀𝙮𝙥𝙯\nORG:Eypz\nTEL;type=CELL;type=VOICE;waid=919747257996:919747257996\nEND:VCARD"
+      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝙀𝙮𝙥𝙯\nORG:Eypz\nTEL;type=CELL;type=VOICE;waid=917994489493:917994489493\nEND:VCARD"
     }]
   },
   'contextInfo': {
@@ -20,8 +20,8 @@ command(
       'body': "𝙀𝙮𝙥𝙯/🌸```",
       'thumbnailUrl': "https://i.imgur.com/AaP7a7m.jpeg",
       'mediaType': 0x1,
-      'mediaUrl': "http://wa.me/+917994489493?text=Hey",
-      'sourceUrl': "http://wa.me/+917994489493?text=Hey",
+      'mediaUrl': "http://wa.me/917994489493",
+      'sourceUrl': "http://wa.me/917994489493",
       'showAdAttribution': false
     }
   }
