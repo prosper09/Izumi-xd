95b6fca692c7c1e438e7820e291a0dc9ad6f0fd8
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-24 18:38:01 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,16 +9,16 @@ command(
   async (message, match, m, client) => {
   const zeta = {
   'contacts': {
-    'displayName': "𝙀𝙮𝙥𝙯",
+    'displayName': "𝐄𝐘𝐏𝐙",
     'contacts': [{
-      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝙀𝙮𝙥𝙯\nORG:Eypz\nTEL;type=CELL;type=VOICE;waid=917994489493:917994489493\nEND:VCARD"
+      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝐄𝐘𝐏𝐙\nORG:Eypz\nTEL;type=CELL;type=VOICE;waid=917994489493:917994489493\nEND:VCARD"
     }]
   },
   'contextInfo': {
     'externalAdReply': {
-      'title': "𝙀𝙮𝙥𝙯 ⛮",
-      'body': "𝙀𝙮𝙥𝙯/🌸```",
-      'thumbnailUrl': "https://i.imgur.com/AaP7a7m.jpeg",
+      'title': "𝗜𝗭𝗨𝗠𝗜 𝗫𝗗🧚‍♂️",
+      'body': "𝐄𝐘𝐏𝐙",
+      'thumbnailUrl': "https://i.imgur.com/Uc0uIkO.jpeg",
       'mediaType': 0x1,
       'mediaUrl': "http://wa.me/917994489493",
       'sourceUrl': "http://wa.me/917994489493",
