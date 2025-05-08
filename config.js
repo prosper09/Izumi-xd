c60c0f712429e5e0aa0dbb27fe0890fe70475224
prosper09 <prospernzvengende@gmail.com>
2025-05-08 15:52:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,7 @@ let HANDLER = "false";
 module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
-  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || " ",
+  ANTILINK_ACTION: process.env.ANTI_LINK || "kick:process.env.SESSION_ID || " ",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[.]',
   BRANCH: "main",
